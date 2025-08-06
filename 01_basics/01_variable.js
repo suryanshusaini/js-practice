32b87ecaf49cc2b143aa0b03c2fb7cc1f4ff814a
suryanshusaini <149190298+suryanshusaini@users.noreply.github.com>
2025-08-06 06:08:07 +0000

    study variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+
+const name = 'Suryanshu saini';
+let age = 19;
+var dob = 2005;
+let gender;
+
+console.log(`${name}`);
+console.log(`${age}`);
+console.log(`${dob}`);
+
+// to print in table format
+console.table([name,age,dob,gender]);
+
+/*
+Prefer not to use var 
+because of blocked scope and functional scope
+*/
