bb2d972681c4eed8fd2435347d877d6895fa88e5
suryanshusaini <149190298+suryanshusaini@users.noreply.github.com>
2025-08-06 06:18:02 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+'use strict';//treat all js code as newer version
+
+// alert(3+3) , we are using node.js not browser
+
+// number => 2 to power 53
+// bigint
+// String => ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof null);
+console.log(typeof undefined);
